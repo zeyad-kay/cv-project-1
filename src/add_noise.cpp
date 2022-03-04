13a587d85230d5db53646a7b1429792ebacaf0eb
ahmedelbadawy <61471534+ahmedelbadawy@users.noreply.github.com>
2022-03-04 23:53:10 +0200

    noise done

--- src/add_noise.cpp
+++ src/add_noise.cpp
@@ -2,28 +2,24 @@
 #include <opencv2/opencv.hpp>
 #include <random>
 
-img::Image add_noise(img::Image source , char type ,uchar noise_factor){
-    cv::Mat img = source.mat.clone();
+    img::Image add_noise(img::Image source , char type ,uchar noise_factor){
+
+    cv::Mat source_mat = source.mat.clone();
+
     img::Image noisy_image;
     std::default_random_engine generator;
     std::normal_distribution<double> gauss_random(0, 1);
     std::uniform_real_distribution<double> uniform_random(-1,1);
     std::uniform_int_distribution<int> sp_random(0,100);
 
-    int channels_number = img.channels();
-    int cols_number = img.cols * channels_number;
-    int rows_number = img.rows;
+    int channels_number = source_mat.channels();
+    int cols_number = source_mat.cols;
+    int rows_number = source_mat.rows;
     double noise = 0.f;
     int random_salt;
-    uchar* row_ptr;
-    if (img.isContinuous())
-  {
-    cols_number = cols_number * rows_number;
-    rows_number = 1;
-  }
+    
     for(int row=0;row<rows_number;row++){
-        row_ptr = img.ptr(row);
-    for(int col=0;col<cols_number;col = col+3){
+    for(int col=0;col<cols_number;col++){
         if(type == 'G'){
             noise = gauss_random(generator)*noise_factor;
         }
@@ -36,18 +32,17 @@ img::Image add_noise(img::Image source , char type ,uchar noise_factor){
         for(int ch=0;ch<channels_number;ch++){
             if (type == 'S'){
                 if(random_salt<noise_factor){
-                    row_ptr[col + ch] =  0;
+                    source_mat.at<cv::Vec3b>(row,col)[ch] =  0;
                     }
                 else if(random_salt>(100-noise_factor)){
-                    row_ptr[col + ch] =  255;
+                    source_mat.at<cv::Vec3b>(row,col)[ch] =  255;
                 }
             }
             else{
-            row_ptr[col + ch] +=  (uchar)noise;
+            source_mat.at<cv::Vec3b>(row,col)[ch] +=  (uchar)noise;
             }
         }
     }
     }
-    noisy_image.mat = img;
-    return noisy_image;
+    return img::Image(source_mat);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -13,46 +13,48 @@ using namespace img;
 int main(int argc, char **argv)
 {
   // Read image
-  Image image = Image("H:/cv-project-1/lennagray.png");
+  Image image = Image("H:/cv-project-1/y.jpg");
+  // image.mat.at<cv::Vec3b>(0,0)[0] = 6;
+  // std::cout << image.mat.at<cv::Vec3b>(0,0) << image.mat.at<cv::Vec3b>(0,1);
   Image noisy = add_noise(image,'S',5);
-  image.display();
   noisy.display();
-  cvector<int> mask = {1, 2, 3, 4};
+  image.display();
+  // cvector<int> mask = {1, 2, 3, 4};
 
-  std::cout << mask << "\n";
-  std::cout << "Squared: "<< (mask * mask) << "\n";
-  std::cout << "Multipled by 2"<< (mask * 2) << "\n";
-  std::cout << "Added by 2"<< (mask + 2) << "\n";
+  // std::cout << mask << "\n";
+  // std::cout << "Squared: "<< (mask * mask) << "\n";
+  // std::cout << "Multipled by 2"<< (mask * 2) << "\n";
+  // std::cout << "Added by 2"<< (mask + 2) << "\n";
   
-  cvector<cvector<int>> mask2d = {
-      {1, 1, 1},
-      {1, 1, 1},
-      {1, 1, 1},
-  };
+  // cvector<cvector<int>> mask2d = {
+  //     {1, 1, 1},
+  //     {1, 1, 1},
+  //     {1, 1, 1},
+  // };
   
-  std::cout << mask << "\n";
-  std::cout << "Squared: "<< (mask2d + mask2d) << "\n";
-  std::cout << "Multipled by 2"<< (mask2d * 2) << "\n";
-  std::cout << "Added by 2"<< (mask2d + 2) << "\n";
+  // std::cout << mask << "\n";
+  // std::cout << "Squared: "<< (mask2d + mask2d) << "\n";
+  // std::cout << "Multipled by 2"<< (mask2d * 2) << "\n";
+  // std::cout << "Added by 2"<< (mask2d + 2) << "\n";
 
-  Image hsv = convert(image, "bgr", "hsv");
+  // Image hsv = convert(image, "bgr", "hsv");
 
-  // return 3 channels of image (h,s,v) or (b,g,r)
-  cvector<Image> splt = split(hsv);
+  // // return 3 channels of image (h,s,v) or (b,g,r)
+  // cvector<Image> splt = split(hsv);
 
-    std::cout << splt.size() << "\n";
+  //   std::cout << splt.size() << "\n";
 
 
-  merge 3 channels of image to 1 image
-  Image merged = merge(splt);
+  // // merge 3 channels of image to 1 image
+  // Image merged = merge(splt);
 
-  Image bgr = convert(merged, "hsv", "bgr");
+  // Image bgr = convert(merged, "hsv", "bgr");
  
-  // bgr.display();
+  // // bgr.display();
   
-  Image gray = convert(merged, "bgr", "gray");
+  // Image gray = convert(merged, "bgr", "gray");
   
-  gray.display();
+  // gray.display();
 
   return 0;
 }
