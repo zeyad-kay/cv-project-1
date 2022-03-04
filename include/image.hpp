ec2f47b455327929da8b04a24b1428069edab055
zeyad <zeyad_khaled@outlook.com>
2022-03-04 19:40:37 +0200

    fixes everything

--- include/image.hpp
+++ include/image.hpp
@@ -1,11 +1,30 @@
 #ifndef __IMAGE_HPP__
 #define __IMAGE_HPP__
 
+#include <opencv2/opencv.hpp>
 #include "cvector.hpp"
+#include <string.h>
 
 namespace img
 {
-    cvector<cvector<int>> filter(cvector<cvector<int>> img, cvector<cvector<int>> mask);
-}
+    class Image
+    {
+        public:
+            cv::Mat mat;
+            cvector<uchar> pixels;
+            Image(){};
+            Image(std::string path);
+            Image(cv::Mat mat);
+            Image(cvector<uchar> pixels, size_t rows, size_t cols, int type);
+            void display() const;
+            void vectorize();
+            cvector<cvector<uchar>> Image::to_2d() const;
+    };
+    Image filter(const Image &img, cvector<cvector<uchar>> mask);
+    Image merge(const cvector<Image> &imgs);
+    Image convert(const Image &img, std::string from, std::string to);
+    cvector<Image> split(const Image &img);
+    Image normalize(Image &img, int min, int max);
+};
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,60 +1,51 @@
 #include <stdio.h>
 #include <opencv2/opencv.hpp>
 #include <iostream>
-#include "utils.hpp"
-#include "tasks.hpp"
+#include "Image.hpp"
 #include "cvector.hpp"
+
 using namespace cv;
+using namespace img;
 
-int main(int argc, char** argv )
+int main(int argc, char **argv)
 {
-    // Read image
-    Mat image;
-    image = read_image();
+  // Read image
+  Image image = Image("C:\\Users\\zeyad\\Desktop\\cv-project-1\\lenna.png");
+  // image.display();
 
-    // split channels     
-    Mat chnnels[3];
-    split(image, chnnels);//splitting images into 3 different channels//  
-    Mat b = chnnels[0];//loading blue channels//
-    Mat g = chnnels[1];//loading green channels//
-    Mat r = chnnels[2];//loading red channels//  
+  cvector<int> mask = {1, 2, 3, 4};
 
-    // vectorization
-    cvector<unsigned char> array =vectorizzation(image);
+  std::cout << mask << "\n";
+  std::cout << "Squared: "<< (mask * mask) << "\n";
+  std::cout << "Multipled by 2"<< (mask * 2) << "\n";
+  std::cout << "Added by 2"<< (mask + 2) << "\n";
+  
+  cvector<cvector<int>> mask2d = {
+      {1, 1, 1},
+      {1, 1, 1},
+      {1, 1, 1},
+  };
+  
+  std::cout << mask << "\n";
+  std::cout << "Squared: "<< (mask2d + mask2d) << "\n";
+  // std::cout << "Multipled by 2"<< (mask2d * 2) << "\n";
+  // std::cout << "Added by 2"<< (mask2d + 2) << "\n";
 
-    // Normalization Task
-    normalize_image(array,100,255);
+  Image hsv = convert(image, "bgr", "hsv");
 
-    // 2d vectorization
-    /*
-    auto N = 3;	// num of rows
-    auto M = 3; // num of cols in each row
-    auto default_value = 1; // default value of all int elements
-    cvector<cvector<int>> matrix(N, cvector<int>(M, default_value));
-    cvector<int>temp(9,0);  //1d vector 
-      temp[0] = 1;
-      temp[1] = 2;
-      temp[2] = 3;
-      temp[3] = 4;
-      temp[4] = 5;
-      temp[5] = 6;
-      temp[6] = 7;
-      temp[7] = 8;
-    vectorization_2d(N,M, temp,matrix);
-    */
-        return 0;
+  // return 3 channels of image (h,s,v) or (b,g,r)
+  cvector<Image> splt = split(hsv);
+
+  // merge 3 channels of image to 1 image
+  Image merged = merge(splt);
+
+  Image bgr = convert(merged, "hsv", "bgr");
+ 
+  // bgr.display();
+  
+  Image gray = convert(merged, "bgr", "gray");
+  
+  // gray.display();
+
+  return 0;
 }
-
-
-
-    // print 2d vector
-    /*
-      for (int i = 0; i < N; i++)
-      {
-          for (int j = 0; j <M; j++)
-          {
-            std::cout << matrix[i][j] << " ";
-          }
-          std::cout << std::endl;
-      }
-      */
