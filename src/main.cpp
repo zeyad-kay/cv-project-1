dcc885d73d9b21bba520b9aae69bc32c23ab3cd4
zeyad <zeyad_khaled@outlook.com>
2022-03-07 20:18:00 +0200

    clear main

--- src/main.cpp
+++ src/main.cpp
@@ -9,12 +9,5 @@ using namespace img;
 
 int main(int argc, char **argv)
 {
-  Image image = Image("images/lenna.png");
-  Image noisy = add_noise(image,"Salt",5);
-  Image filtered = noise_filter(noisy,3,"Median");
-
-  image.display("image");
-  noisy.display("noisy");
-  filtered.display("filtered");
   return 0;
 }
